c85d42c84cbcf0acb0c7777a693731b579304823
Ali Akber <ali852609@gmail.com>
2023-11-01 19:21:23 +0100

    Create search_insert_position.dart

---
+++ easy/array/search_insert_position.dart
@@ -0,0 +1,24 @@
+class Solution {
+  int searchInsert(List<int> nums, int target) {
+      int low = 0;
+      int high = nums.length-1;
+      int mid = 0;
+
+      if(nums[high] < target) return high+1;
+
+      while(low <= high) {
+          mid = low + ((high-low)/2).toInt();
+
+          if(nums[mid]==target) {
+              return mid;
+          }  
+
+          if(nums[mid] > target) {
+            high = mid-1;
+          }else {
+            low = mid+1;
+          }
+      }
+      
+      return low;
+}
